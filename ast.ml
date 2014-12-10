d1f2e41870f39e8720f41fb2d221986e3e80dfbd
Alisha Sindhwani <sindhwani.alisha@gmail.com>
2014-12-10 18:31:57 -0500

    new make file

--- ast.ml
+++ ast.ml
@@ -45,12 +45,100 @@ type parameter = {
   ptype : types;
 }
 
-type func_decl = {
+type func = {
     ret_type : types;
     fname : string;
     formals : parameter list;
     locals : variable list;
     body : stmt list;
   }
+  
+type decl = 
+    Func_Decl of scope_func_decl
+  | Var_Decl of scope_var_decl
 
-type program = variable list * func_decl list
+type program = variable list * func list
+
+
+let string_of_types = function
+  Bool_Type -> "bool"
+  | Int_Type -> "int"
+  | Pitch_Type -> "pitch"
+  | String_Type -> "string"
+  | Frac_Type -> "frac"
+  | Rhythm_Type -> "rhythm"
+  | Duration_Type -> "duration"
+  | Chord_Type -> "chord"
+  | Track_Type -> "track"
+  | Composition_Type -> "composition"
+
+let string_of_binop = function
+    Add -> "+" 
+  | Sub -> "-" 
+  | Mult -> "*" 
+  | Div -> "/" 
+  | Equal -> "==" 
+  | Neq -> "!="
+  | Less -> "<" 
+  | Leq -> "<=" 
+  | Greater -> ">" 
+  | Geq -> ">="
+
+
+let rec string_of_expr = function
+  Bool_Lit(b) -> string_of_bool b
+  | Int_Lit(i) -> string_of_int i
+  | String_Lit(s) -> s
+  | Frac_Lit(n, d) -> "$" ^ string_of_int n ^ "/" ^ string_of_int d ^ "$"
+  | Array_Lit(e) -> String.concat ", " (List.map string_of_expr e) 
+  | Id(s) -> s 
+  | Binop(e1, o, e2) ->
+      string_of_expr e1 ^ " " ^
+      string_of_binop o ^ " " ^
+      string_of_expr e2
+  |  Create(t, id, rhs) -> string_of_types t ^ " " ^ id ^ " = " ^ string_of_expr rhs 
+  |  Assign(id, rhs) -> id ^ " = " ^
+    string_of_expr rhs
+  | Tuple(e1, e2) -> "(" ^ string_of_expr e1 ^ ", " ^ string_of_expr e2 ^ ")"
+  | Call(f, e) -> 
+    f ^ "(" ^ String.concat ", " (List.map string_of_expr e) ^ ")"
+  | Noexpr -> ""
+
+(* let string_of_elif (expr, stmt) = 
+  "elif (" ^ string_of_expr expr ^ ") { \n" ^
+  string_of_stmt stmt ^ "\n}\n" 
+ 
+let string_of_elseifs elseifs = 
+  String.concat "" (List.map (function(expr, stmt) -> string_of_expr expr ^ string_of_stmt stmt) elseifs) ^ "\n"  *)
+
+let rec string_of_stmt = function
+  Block(stmts) ->
+    "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n"
+  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n"
+  | If(e, s, ei, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s ^
+    String.concat "" (List.map (function(expr, stmt) -> 
+      "elif (" ^ string_of_expr expr ^ ") {\n" ^ string_of_stmt stmt ^ "\n}\n") ei) ^ "\n" 
+  | If(e, s1, ei, s2) -> "if (" ^ string_of_expr e ^ ")\n" ^
+    string_of_stmt s1 ^
+    String.concat "" (List.map (function(expr, stmt) -> 
+      "elif (" ^ string_of_expr expr ^ ") {\n" ^ string_of_stmt stmt ^ "\n}\n") ei) ^ "\n" ^
+    "else\n" ^ string_of_stmt s2
+  | For(e1, e2, e3, s) ->
+      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
+      string_of_expr e3  ^ ") " ^ string_of_stmt s
+  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+
+let string_of_vdecl vdecl = string_of_types vdecl.vtype ^ " " ^ vdecl.vname ^ 
+              " = " ^ string_of_expr vdecl.vexpr ^ ";\n"  
+let string_of_pdecl pdecl = string_of_types pdecl.ptype ^ " " ^ pdecl.pname 
+
+let string_of_fdecl fdecl =
+  string_of_types fdecl.ret_type ^ " " ^ fdecl.fname ^ "(" ^ String.concat ", " (List.map string_of_pdecl fdecl.formals) ^ ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
+
+let string_of_program (vars, funcs) =
+  String.concat "" (List.map string_of_vdecl (List.rev vars) ) ^ "\n" ^
+  String.concat "\n" (List.map string_of_fdecl (List.rev funcs) ) ^ "\n"
--- symtab.ml
+++ symtab.ml
@@ -1,5 +1,4 @@
 open Ast
-open Interpreter
 
 (* 
  * SymMap contains string : Ast.decl pairs representing
@@ -22,6 +21,7 @@ let rec symtab_add_decl (name:string) (decl:decl) env =
  * recursively add list of variables to the symbol table along with the scope of
  * the block in which they were declared
  *) 
+
 let rec symtab_add_vars (vars:var list) env =
     match vars with
       [] -> env
@@ -41,7 +41,7 @@ let rec symtab_add_stmts (stmts:stmt list) env =
         | _ -> env) in symtab_add_stmts tail env (* return, continue, break, etc *)
 
 
-and symtab_add_block (b:block) env =
+let symtab_add_block (b:block) env =
     let (table, scope) = env in 
     let env = symtab_add_vars b.locals (table, b.block_id) in 
     let env = symtab_add_stmts b.statements env in 
@@ -49,7 +49,7 @@ and symtab_add_block (b:block) env =
     ((fst env), scope) (* return what we've made *)
 
 
-and symtab_add_func (f:func) env =
+let symtab_add_func (f:func) env =
     let scope = snd env in
     let args = List.map snd f.formals in (* gets name of every formal *)
     let env = symtab_add_decl f.fname (Func_Decl(f.fname, f.ret_type, args, scope)) env in (* add current function to table *)
@@ -58,7 +58,7 @@ and symtab_add_func (f:func) env =
 
 
 (* add list of functions to the symbol table *)
-and symtab_add_funcs (funcs:func list) env =
+let symtab_add_funcs (funcs:func list) env =
     match funcs with
        [] -> env
      | head :: tail -> let env = symtab_add_func head env in 
@@ -72,5 +72,4 @@ let add_builtins env =
 let symtab_of_program (p:Ast.program) =
     let env = add_builtins (SymTable.empty, 0) in
     let env = symtab_add_vars (fst p) env in
-    symtab_add_funcs (snd p) env
-
+    symtab_add_funcs (snd p) env 
