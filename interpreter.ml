1086cf30cfe3d46ea21c71d357bf91537502bd49
Melissa O'Sullivan <mko2110@columbia.edu>
2014-12-10 20:51:40 -0500

    added testing script

--- interpreter.ml
+++ interpreter.ml
@@ -70,4 +70,4 @@ let string_of_program (vars, funcs) =
 let _ =
 	let lexbuf = Lexing.from_channel stdin in
 	let program = Parser.program Scanner.token lexbuf in
-	print_string (string_of_program program) 
+	prerr_string (string_of_program program) 
---
+++ runtests.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+tests=$(find tests -name *\.corg)
+had_failures="0"
+tmp_file=".test_out"
+suffix=".out"
+
+get_test_name () {
+    local fullpath=$1
+    testpath="${fullpath%.*}"
+    test_name="${testpath##*/}"
+
+}
+
+for file in $tests
+do
+    get_test_name "$file"
+
+    ./interpret < "$file" 2> ".test_out"
+
+    if [[ ! $(diff ".test_out" "$testpath$suffix") ]]
+    then
+        echo "success: $test_name"
+    else
+        echo "FAIL:    $test_name"
+        had_failures="1"
+
+        printf "Expected: {\n"
+        cat "$testpath$suffix"
+        printf "}\n"
+        echo
+
+        printf "Recieved: {\n"
+        cat "$.test_out"
+        printf "}\n"
+        echo
+    fi
+done
+
+rm -f "$tmp_file"
+exit $had_failures
