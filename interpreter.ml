01574a39233eb9bb3ebb33de401ee4c5ca196aca
Justin Zhao <justin.zhao71@gmail.com>
2014-12-11 17:31:18 -0500

    Updated interpreter with symbol table

--- interpreter.ml
+++ interpreter.ml
@@ -3,4 +3,7 @@ open Ast
 let _ =
 	let lexbuf = Lexing.from_channel stdin in
 	let program = Parser.program Scanner.token lexbuf in
-	prerr_string (string_of_program program) 
+    let env = Symtab.symtab_of_program program in
+    prerr_string (string_of_program program) 
+    
+    (*print_string (Symtab.string_of_symtab env)*)
