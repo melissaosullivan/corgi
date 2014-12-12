63b0ef3eaf79e23b39e0f024754173817837f4d8
Justin Zhao <justin.zhao71@gmail.com>
2014-12-11 20:38:33 -0500

    Added check functionality

--- interpreter.ml
+++ interpreter.ml
@@ -4,6 +4,8 @@ let _ =
 	let lexbuf = Lexing.from_channel stdin in
 	let program = Parser.program Scanner.token lexbuf in
     let env = Symtab.symtab_of_program program in
-    prerr_string (string_of_program program) 
+    let checked = Check.check_program program env in
+    ignore checked; print_string "Passed Semantic Analysis.\n"
     
-    (*print_string (Symtab.string_of_symtab env)*)
+    (*prerr_string (string_of_program program) *)
+    (*print_string (Symtab.string_of_symtab env) *)
