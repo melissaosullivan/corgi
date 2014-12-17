c12178fe60bd1f199ef1214a725f664bfd5193b1
Justin Zhao <justin.zhao71@gmail.com>
2014-12-17 00:18:45 -0500

    filler populated test files

---
+++ tests/intermedout/op_chord.java
@@ -0,0 +1,1 @@
+Fatal error: exception Failure("symbol c1 must be an array, index must be of type int")
---
+++ tests/intermedout/op_track.java
@@ -0,0 +1,1 @@
+Fatal error: exception Failure("symbol t must be an array, index must be of type int")
