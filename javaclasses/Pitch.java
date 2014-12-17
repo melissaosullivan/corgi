1f6d705a5defe9dfec45a8a1fb9fb869687046ca
Justin Zhao <justin.zhao71@gmail.com>
2014-12-17 06:22:24 -0500

    Temporary port fix for using no jar

--- javaclasses/Pitch.java
+++ javaclasses/Pitch.java
@@ -1,29 +1,31 @@
-
-public class Pitch implements Comparable<Pitch>{
+public class Pitch implements Comparable<Pitch> {
 
 	public int pitch;
-	
-	public Pitch(int p){
+
+	public Pitch(int p) {
 		pitch = p;
 	}
-	public Pitch(Pitch p){
+
+	public Pitch(Pitch p) {
 		pitch = p.pitch;
 	}
-	
-	public void add(Pitch p){
-		pitch = pitch + p.pitch; 
+
+	public void add(Pitch p) {
+		pitch = pitch + p.pitch;
 	}
-	
-	public void subtract(Pitch p){
+
+	public void subtract(Pitch p) {
 		pitch = pitch - p.pitch;
 	}
-	
-	public void multiply(Pitch p){
+
+	public void multiply(Pitch p) {
 		pitch = pitch * p.pitch;
 	}
-	public void divide(Pitch p){
+
+	public void divide(Pitch p) {
 		pitch = pitch / p.pitch;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -31,6 +33,7 @@ public class Pitch implements Comparable<Pitch>{
 		result = prime * result + pitch;
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -44,14 +47,22 @@ public class Pitch implements Comparable<Pitch>{
 			return false;
 		return true;
 	}
+
 	@Override
 	public int compareTo(Pitch o) {
 		return pitch - o.pitch;
 	}
-	
-	public String toString() {
-		return Utils.intToNoteString(pitch);
+
+	public final static String[] NOTENAMES = { "C", "Db", "D", "Eb", "E", "F",
+			"F#", "G", "Ab", "A", "Bb", "B" };
+
+	public static String intToNoteString(int n) {
+		int octave = n / 12;
+		return NOTENAMES[n % 12] + Integer.toString(octave);
 	}
 
-	
+	public String toString() {
+		return intToNoteString(pitch);
+	}
+
 }
