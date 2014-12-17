4774c17ebacd7a2ffdb073a6829ac08479c0d42d
Alisha Sindhwani <sindhwani.alisha@gmail.com>
2014-12-17 06:37:57 -0500

    added static to global variables and checking for arrays in var decls

--- javagen.ml
+++ javagen.ml
@@ -141,11 +141,25 @@ and tostring_str dexpr =
 
 	
 
-let write_scope_var_decl svd =
-	let (n, b, t, _) = svd in write_type t ^ " " ^ n ^ ";\n"
-
 let write_scope_var_decl_func svd =
-	let (n, b, t, _) = svd in write_type t ^ " " ^ n
+	let (n, b, t, _) = svd in 
+		match b with 
+		  true -> write_type t ^ "[]" ^ n (* true if it is an array *)
+		| false -> write_type t ^ " " ^ n
+
+let write_scope_var_decl svd =
+	write_scope_var_decl_func svd ^ ";\n"
+
+let write_global_scope_var_decl gsvd = 
+	"static " ^ write_scope_var_decl_func gsvd ^ ";\n"
+
+(*let write_scope_var_decl svd =
+	let (n, b, t, _) = svd in 
+		match b with 
+		  true -> write_type t ^ "[]" ^ n ^ ";\n" (* true if it is an array *)
+		| false -> write_type t ^ " " ^ n ^ ";\n" *)
+
+
 
 let write_assign name dexpr t =
 	(match t with
@@ -159,7 +173,7 @@ let rec write_stmt = function
 	| D_Assign (name, dexpr, t) -> write_assign name dexpr t ^ ";\n"
 	| D_Return(dexpr) -> "return " ^ write_expr dexpr ^ ";\n"
     | D_If(dexpr, dstmt1, dstmt2) -> "if(" ^ write_expr dexpr ^  ")" ^  write_stmt dstmt1 ^ "else"  ^ write_stmt dstmt2
-    | D_For(dstmt1, dstmt2, dstmt3, dblock) -> "for(" ^ write_stmt dstmt1  ^ write_stmt dstmt2 ^ (remove_semi (write_stmt dstmt3)) ^ ")" 
+    | D_For(dstmt1, dstmt2, dstmt3, dblock) -> "for(" ^ write_stmt dstmt1  ^ write_stmt dstmt2 ^ (write_stmt dstmt3) ^ ")" 
     | D_While(dexpr, dblock) -> "while(" ^ write_expr dexpr ^ ")"  ^ write_block dblock
 	
 and write_block dblock =
@@ -171,4 +185,4 @@ let write_func dfunc =
 	| _ -> "static " ^ write_type dfunc.d_ret_type ^ " " ^ dfunc.d_fname ^ "("  ^ String.concat "," (List.map write_scope_var_decl_func dfunc.d_formals) ^ ")" ^ write_block dfunc.d_fblock
 
 let write_pgm pgm = 
-	 "public class Intermediate {\n" ^ String.concat "\n" (List.map write_scope_var_decl pgm.d_gvars) ^ String.concat "\n" (List.map write_func pgm.d_pfuncs) ^ "}"
+	 "public class Intermediate {\n" ^ String.concat "\n" (List.map write_global_scope_var_decl pgm.d_gvars) ^ String.concat "\n" (List.map write_func pgm.d_pfuncs) ^ "}"
